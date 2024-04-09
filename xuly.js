260b8128debcf538cd54bc843985b179bb3e9583
Khanh <kduong9669@gmail.com>
2024-04-10 00:59:28 +0700

    kaine

--- xuly.js
+++ xuly.js
@@ -1,6 +1,20 @@
-let list = document.querySelectorAll(".item-header");
-[...list].forEach(item => console.log(item));
-[...list].forEach(item => item.addEventListener("click",function(e){
-  console.log(e.taregt);
-}))
+
+document.getElementById('scroll_to_back').addEventListener('click', function() {
+  scrollToTop();
+});
+window.addEventListener('scroll', function() {
+  var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+  if (scrollPosition > 100) {
+      document.getElementById('scroll_to_back').style.display = 'block';
+  } else {
+      document.getElementById('scroll_to_back').style.display = 'none';
+  }
+});
+function scrollToTop() {
+  var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+  if (currentScroll > 0) {
+      window.requestAnimationFrame(scrollToTop);
+      window.scrollTo(0, currentScroll - (currentScroll / 7)); 
+  }
+}
 
